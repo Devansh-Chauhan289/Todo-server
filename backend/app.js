93c282fe957c26483d4a51ba3b14c54e486ae2f4
Devansh-Chauhan289 <chauhanmansi628@gmail.com>
2025-05-19 10:21:54 +0530

    Remove unnecessary CORS origin and options for cleaner configuration

--- backend/app.js
+++ backend/app.js
@@ -25,17 +25,17 @@ const REDIS_KEY = "FULLSTACK_TASK_DEVANSH_CHAUHAN";
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: ["http://localhost:5173","https://ping-me-frt.vercel.app"],
+    origin: ["http://localhost:5173"],
     methods: ["GET", "POST"],
     credentials: true,
 }));
-app.options('*', cors());
+
 
 app.use("/task", TaskRouter);
 
 const io = new Server(httpServer, {
     cors: {
-        origin: ["http://localhost:5173","https://ping-me-frt.vercel.app"],
+        origin: ["http://localhost:5173"],
         methods: ["GET", "POST"],
         credentials: true,
     },
